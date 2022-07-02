331fc49c60a261435f7a3aef71bcaf4447cacca4
Lorenzo Bianconi <lorenzo@kernel.org>
2022-07-02 18:53:16 +0530

    libbpf: add API to get XDP/XSK supported features
    Extend bpf_xdp_query routine in order to get XDP/XSK supported features
    of netdev over route netlink interface.
    
    Co-developed-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Co-developed-by: Marek Majtyka <alardam@gmail.com>
    Signed-off-by: Marek Majtyka <alardam@gmail.com>

--- tools/lib/bpf/nlattr.h
+++ tools/lib/bpf/nlattr.h
@@ -14,6 +14,7 @@
 #include <errno.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
+#include <linux/genetlink.h>
 
 /* avoid multiple definition of netlink features */
 #define __LINUX_NETLINK_H
@@ -58,6 +59,7 @@ struct libbpf_nla_req {
 	union {
 		struct ifinfomsg ifinfo;
 		struct tcmsg tc;
+		struct genlmsghdr gnl;
 	};
 	char buf[128];
 };
@@ -87,6 +89,11 @@ static inline void *libbpf_nla_data(const struct nlattr *nla)
 static inline uint8_t libbpf_nla_getattr_u8(const struct nlattr *nla)
 {
 	return *(uint8_t *)libbpf_nla_data(nla);
+}
+
+static inline uint16_t libbpf_nla_getattr_u16(const struct nlattr *nla)
+{
+	return *(uint16_t *)libbpf_nla_data(nla);
 }
 
 static inline uint32_t libbpf_nla_getattr_u32(const struct nlattr *nla)
